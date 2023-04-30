d0e2c5fe5ac1880b61fbc3eef15a3c4f8ae82fbb
Jbgamehead <burton.jhordane@student.ccm.edu>
2023-04-30 18:36:18 -0400

    wow

--- frontend/src/EmployeeProfile.jsx
+++ frontend/src/EmployeeProfile.jsx
@@ -4,8 +4,18 @@ import './App.css'
 
 function EmployeeProfile() {
     return (
-        <div className="col-md-6">
-            <img src="./src/assets/um.png" className="w-75" />
+        <div className="d-flex flex-coloumn align-items-center mt-4 px-5 pt-3">
+            <div className="mt-4 px-5 pt-3">
+                <form class="row g-3 w-75">
+
+
+                    <img
+                        src="../src/assets/maxresdefault.jpg"
+                        className="w-75"
+                        style={{ width: 400, height: 400, borderRadius: 400 / 2 }}
+                    />
+                </form>
+            </div>
         </div>
     )
 }
